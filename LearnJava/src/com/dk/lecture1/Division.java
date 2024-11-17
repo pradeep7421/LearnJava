3c57ce65f004aed7134c034ffa119c947cb6d1e7
deepali Kushwaha <deepalikushwahadk@gmail.com>
2024-11-17 11:27:06 +0530

    kindly review

--- LearnJava/src/com/dk/lecture1/Division.java
+++ LearnJava/src/com/dk/lecture1/Division.java
@@ -3,8 +3,19 @@ package com.dk.lecture1;
 public class Division {
 
 	public static void main(String[] args) {
-		
-
+//		int x = 99;
+//		int y = 11;
+//		int div = x / y;
+//		System.out.println(div);
+//		}
+//		int x = 9848;
+//		int y = 2;
+//		int div = x / y; 
+//		System.out.println(div);
+		int a = 100000000;
+	    int b = 5;
+	    int div = a / b ;
+	    System.out.println (div);
+		}
 	}
 
-}
